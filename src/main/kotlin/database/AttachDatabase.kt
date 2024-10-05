f61e8ad7a5a345f840860f63d53df0a7aa7ce16a
Kiooeht <kiooeht@gmail.com>
2024-10-04 17:36:30 -0700

    Make test command not call relic onEquip

--- src/main/kotlin/database/AttachDatabase.kt
+++ src/main/kotlin/database/AttachDatabase.kt
@@ -12,7 +12,6 @@ import com.google.gson.reflect.TypeToken
 import com.megacrit.cardcrawl.characters.AbstractPlayer
 import com.megacrit.cardcrawl.core.AbstractCreature
 import com.megacrit.cardcrawl.core.CardCrawlGame
-import com.megacrit.cardcrawl.core.Settings
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 import com.megacrit.cardcrawl.helpers.RelicLibrary
 import com.megacrit.cardcrawl.relics.AbstractRelic
@@ -69,10 +68,7 @@ object AttachDatabase {
         val character = AbstractDungeon.player?.chosenClass ?: return
         database.getOrDefault(character, mutableMapOf()).keys.forEach { id ->
             RelicLibrary.getRelic(id)?.makeCopy()?.let { relic ->
-                AbstractDungeon.getCurrRoom().spawnRelicAndObtain(
-                    Settings.WIDTH / 2f, Settings.HEIGHT / 2f,
-                    relic
-                )
+                relic.instantObtain(AbstractDungeon.player, AbstractDungeon.player.relics.size, false)
             }
         }
     }
