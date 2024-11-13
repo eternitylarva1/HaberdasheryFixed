a1e6fba739d74bc62d4bc183b1e7b67a73f4ad32
Kiooeht <kiooeht@gmail.com>
2024-11-12 21:19:36 -0800

    Fancy dependency finding

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,43 @@
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import java.util.jar.JarFile
+
 plugins {
     `java-library`
     kotlin("jvm") version "1.7.22"
 }
 
+val modID = "haberdashery"
+group = "com.evacipated.cardcrawl.mod"
+version = "0.1"
+description = "Haberdashery"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+val steamPath = project.properties["sts.steamPath"] ?: "C:/Program Files (x86)/Steam/steamapps"
+val localLibPath = project.properties["sts.localLibPath"]
+val modsPath = project.properties["sts.modsPath"] ?: "$steamPath/common/SlayTheSpire/mods"
+
+extra.apply {
+    mapOf(
+        "id" to modID,
+        "name" to description,
+        "version" to version,
+        "sts_version" to "12-22-2020",
+        "mts_version" to "3.21.0",
+        "authors" to listOf("kiooeht")
+            .joinToString(separator = "\", \""),
+        "dependencies" to listOf("basemod")
+            .joinToString(separator = "\", \""),
+    ).forEach { (k, v) -> set(k, v) }
+}
+
 buildscript {
-    extra.apply {
-        mapOf(
-            "id" to "haberdashery",
-            "name" to "Haberdashery",
-            "version" to "0.1",
-            "sts_version" to "12-22-2020",
-            "mts_version" to "3.21.0",
-            "authors" to listOf("kiooeht").joinToString(separator = "\", \""),
-            "dependencies" to listOf("basemod").joinToString(separator = "\", \""),
-        ).forEach { (k, v) -> set(k, v) }
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.google.code.gson:gson:2.11.0")
     }
 }
 
@@ -24,27 +48,20 @@ repositories {
 
 dependencies {
     implementation(kotlin("reflect"))
-    val libDir = layout.projectDirectory.dir("..").dir("lib")
-    implementation(files(
-        libDir.file("desktop-1.0.jar"),
-        libDir.file("ModTheSpire.jar"),
-        libDir.file("BaseMod.jar"),
-    ))
+    implementation(sts())
+    implementation(sts("modthespire"))
+    implementation(sts("basemod"))
 }
 
-group = "com.evacipated.cardcrawl.mod"
-version = extra["version"].toString()
-description = extra["name"].toString()
-java.sourceCompatibility = JavaVersion.VERSION_1_8
-
 tasks.processResources {
+    filteringCharset = "UTF-8"
     val expansion: FileCopyDetails.() -> Unit = {
         expand(
             "mod" to project.extra.properties,
         )
     }
     filesMatching("ModTheSpire.json", expansion)
-    filesMatching("${project.extra["id"]}Assets/**/*.json", expansion)
+    filesMatching("${modID}Assets/**/*.json", expansion)
 }
 
 tasks.jar {
@@ -58,5 +75,48 @@ tasks.jar {
 
 tasks.register<Copy>("copyJarToMods") {
     from(layout.buildDirectory.dir("libs"))
-    into(layout.projectDirectory.dir("..").dir("_ModTheSpire").dir("mods"))
+    into(modsPath)
+}
+
+// =============================================
+// Helper functions for finding mod dependencies
+// =============================================
+fun DependencyHandler.sts(modID: String? = null) = run {
+    if (modID == null) {
+        files("$steamPath/common/SlayTheSpire/desktop-1.0.jar")
+    } else {
+        val gson = GsonBuilder().create()
+        val libTree = localLibPath?.let { fileTree(it).apply {
+            include("*.jar")
+        } }
+        val steamTree = fileTree("$steamPath/workshop/content/646570").apply {
+            include("*/*.jar")
+        }
+        val tree = if (libTree != null) {
+            libTree + steamTree
+        } else {
+            steamTree
+        }
+
+        val files = tree.filter { file ->
+            val jar = JarFile(file)
+            val mtsJson = jar.getEntry("ModTheSpire.json")
+            if (mtsJson != null) {
+                val map = gson.fromJson<Map<String, Any?>>(
+                    jar.getInputStream(mtsJson).reader(),
+                    object : TypeToken<Map<String, Any?>>() {}.type
+                )
+                map["modid"] == modID
+            } else {
+                false
+            }
+        }
+        files(files.first())
+    }
+}
+
+fun DependencyHandler.sts(steamID: Int) = run {
+    fileTree("$steamPath/workshop/content/646570/$steamID").apply {
+        include("*.jar")
+    }
 }
