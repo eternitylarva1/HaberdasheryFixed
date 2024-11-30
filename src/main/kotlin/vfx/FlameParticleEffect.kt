6fee4c58788b472b4f2a16a898b69085c578491a
Kiooeht <kiooeht@gmail.com>
2024-11-29 19:20:50 -0800

    Add flame particles to Blue Candle

---
+++ src/main/kotlin/vfx/FlameParticleEffect.kt
@@ -0,0 +1,21 @@
+package haberdashery.vfx
+
+import com.badlogic.gdx.math.MathUtils
+import com.megacrit.cardcrawl.vfx.scene.TorchParticleSEffect
+import haberdashery.extensions.scale
+import haberdashery.extensions.setPrivate
+
+class FlameParticleEffect(x: Float, y: Float) : TorchParticleSEffect(setNotGreen(x), y) {
+    init {
+        val vY = MathUtils.random(7f, 40f).scale()
+        setPrivate("vY", vY, clazz = TorchParticleSEffect::class.java)
+        renderBehind = false
+    }
+
+    companion object {
+        private fun setNotGreen(x: Float): Float {
+            renderGreen = false
+            return x
+        }
+    }
+}
