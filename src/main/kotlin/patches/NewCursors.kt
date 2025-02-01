3e023e4b837d62bedfbf633ad8bd673a9ed69605
Kiooeht <kiooeht@gmail.com>
2025-01-31 22:09:41 -0800

    Custom cursor system allowing hotspot specification

--- src/main/kotlin/patches/NewCursors.kt
+++ src/main/kotlin/patches/NewCursors.kt
@@ -1,19 +1,16 @@
 package haberdashery.patches
 
-import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.evacipated.cardcrawl.modthespire.lib.*
 import com.megacrit.cardcrawl.core.CardCrawlGame
 import com.megacrit.cardcrawl.core.GameCursor
 import com.megacrit.cardcrawl.core.GameCursor.CursorType
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 import com.megacrit.cardcrawl.helpers.Hitbox
-import com.megacrit.cardcrawl.helpers.ImageMaster
 import com.megacrit.cardcrawl.helpers.input.InputHelper
 import com.megacrit.cardcrawl.relics.AbstractRelic
 import haberdashery.HaberdasheryMod
 import haberdashery.extensions.inst
-import haberdashery.extensions.scale
+import haberdashery.ui.CustomCursor
 import haberdashery.ui.CustomizeAttachmentsScreen
 import javassist.CtBehavior
 import javassist.expr.ExprEditor
@@ -28,8 +25,8 @@ object NewCursors {
         method = SpirePatch.CLASS
     )
     object Fields {
-        @JvmField val imgHand = SpireField { ImageMaster.loadImage(HaberdasheryMod.assetPath("images/cursorHand.png")) }
-        @JvmField val imgGrab = SpireField { ImageMaster.loadImage(HaberdasheryMod.assetPath("images/cursorGrab.png")) }
+        @JvmField val imgHand = SpireField { CustomCursor(HaberdasheryMod.assetPath("images/cursors/cursorHand.cursor")) }
+        @JvmField val imgGrab = SpireField { CustomCursor(HaberdasheryMod.assetPath("images/cursors/cursorGrab.cursor")) }
     }
 
     @SpirePatch2(
@@ -37,35 +34,17 @@ object NewCursors {
         method = "render"
     )
     object Render {
-        private val SHADOW_OFFSET_X by lazy { (-10).scale() }
-        private val SHADOW_OFFSET_Y by lazy { 8.scale() }
-
         @JvmStatic
         @SpireInsertPatch(
             locator = Locator::class
         )
-        fun render(__instance: GameCursor, ___type: CursorType, sb: SpriteBatch, ___SHADOW_COLOR: Color) {
-            val img = when (___type) {
+        fun render(__instance: GameCursor, ___type: CursorType, sb: SpriteBatch) {
+            val cursor = when (___type) {
                 HAND -> Fields.imgHand.get(__instance)
                 GRAB -> Fields.imgGrab.get(__instance)
                 else -> return
             }
-            sb.color = ___SHADOW_COLOR
-            sb.draw(
-                img,
-                InputHelper.mX - 32f - SHADOW_OFFSET_X,
-                InputHelper.mY - 32f - SHADOW_OFFSET_Y,
-                img.width.scale()*0.8f,
-                img.height.scale()*0.8f,
-            )
-            sb.color = Color.WHITE
-            sb.draw(
-                img,
-                InputHelper.mX - 32f,
-                InputHelper.mY - 32f,
-                img.width.scale()*0.8f,
-                img.height.scale()*0.8f,
-            )
+            cursor.render(sb, InputHelper.mX, InputHelper.mY)
         }
 
         private class Locator : SpireInsertLocator() {
---
+++ src/main/kotlin/ui/CustomCursor.kt
@@ -0,0 +1,141 @@
+package haberdashery.ui
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.files.FileHandle
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.utils.GdxRuntimeException
+import com.badlogic.gdx.utils.StreamUtils
+import com.megacrit.cardcrawl.core.Settings
+import com.megacrit.cardcrawl.helpers.ImageMaster
+import haberdashery.extensions.scale
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+class CustomCursor(cursorFile: FileHandle, imagesDir: FileHandle) {
+    constructor(cursorFile: FileHandle) : this(cursorFile, cursorFile.parent())
+    constructor(internalCursorFile: String) : this(Gdx.files.internal(internalCursorFile))
+
+    private var tex: Texture? = null
+    private var width = 0f
+    private var height = 0f
+    private val hotspot = Vector2()
+
+    init {
+        load(cursorFile, imagesDir)
+    }
+
+    private fun load(cursorFile: FileHandle, imagesDir: FileHandle) {
+        val reader = BufferedReader(InputStreamReader(cursorFile.read()), 64)
+
+        try {
+            while (true) {
+                val line = reader.readLine() ?: break
+
+                if (line.trim().isEmpty()) {
+                    continue
+                }
+
+                if (tex == null) {
+                    val imageFile = imagesDir.child(line)
+                    tex = Texture(imageFile).apply {
+                        setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear)
+                        this@CustomCursor.width = width.toFloat()
+                        this@CustomCursor.height = height.toFloat()
+                    }
+                } else {
+                    with(readTuple(line)) {
+                        when (name) {
+                            "size" -> if (size == 2) {
+                                width = data[0].toInt().toFloat()
+                                height = data[1].toInt().toFloat()
+                            }
+                            "hotspot" -> if (size == 2) {
+                                hotspot.x = data[0].toInt().toFloat()
+                                hotspot.y = data[1].toInt().toFloat()
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            throw GdxRuntimeException("Error reading cursor file: $cursorFile", e)
+        } finally {
+            StreamUtils.closeQuietly(reader)
+        }
+    }
+
+    fun render(sb: SpriteBatch, x: Int, y: Int) {
+        render(sb, x.toFloat(), y.toFloat())
+    }
+
+    fun render(sb: SpriteBatch, x: Float, y: Float) {
+        sb.color = SHADOW_COLOR
+        sb.draw(
+            tex,
+            x - hotspot.x - SHADOW_OFFSET_X,
+            y + hotspot.y - height - SHADOW_OFFSET_Y,
+            width,
+            height,
+        )
+        sb.color = Color.WHITE
+        sb.draw(
+            tex,
+            x - hotspot.x,
+            y + hotspot.y - height,
+            width,
+            height,
+        )
+
+        if (Settings.isDebug) {
+            sb.color = Color.RED
+            sb.draw(
+                ImageMaster.WHITE_SQUARE_IMG,
+                x - 1, y - 1, 2f, 2f
+            )
+        }
+    }
+
+    companion object {
+        private val SHADOW_COLOR by lazy { Color(0f, 0f, 0f, 0.15f) }
+        private val SHADOW_OFFSET_X by lazy { (-10).scale() }
+        private val SHADOW_OFFSET_Y by lazy { 8.scale() }
+
+        private fun readTuple(line: String): Tuple {
+            val colon = line.indexOf(':')
+            if (colon == -1) {
+                throw GdxRuntimeException("Invalid line: $line")
+            } else {
+                val tuple = Tuple(line.substring(0, colon))
+                var lastMatch = colon + 1
+                var i = 0
+
+                while (i < 3) {
+                    val comma = line.indexOf(',', lastMatch)
+                    if (comma == -1) {
+                        break
+                    }
+
+                    tuple.add(line.substring(lastMatch, comma).trim())
+                    lastMatch = comma + 1
+                    i++
+                }
+                tuple.add(line.substring(lastMatch).trim())
+                return tuple
+            }
+        }
+    }
+
+    private data class Tuple(val name: String) {
+        val data = Array(4) { "" }
+        var size = 0
+            private set
+
+        fun add(str: String) {
+            data[size] = str
+            size++
+        }
+    }
+}
