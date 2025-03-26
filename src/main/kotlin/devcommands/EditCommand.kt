00d68ae2a759dc3278ff6217855b7bd7cd3c6809
Kiooeht <kiooeht@gmail.com>
2025-03-25 18:45:49 -0700

    Make edit command suggest all relics that character has attachments for

--- src/main/kotlin/devcommands/EditCommand.kt
+++ src/main/kotlin/devcommands/EditCommand.kt
@@ -3,6 +3,7 @@ package com.evacipated.cardcrawl.mod.haberdashery.devcommands
 import basemod.devcommands.ConsoleCommand
 import basemod.devcommands.relic.Relic
 import com.evacipated.cardcrawl.mod.haberdashery.AdjustRelic
+import com.evacipated.cardcrawl.mod.haberdashery.database.AttachDatabase
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 
 class EditCommand : ConsoleCommand() {
@@ -24,9 +25,7 @@ class EditCommand : ConsoleCommand() {
 
     override fun extraOptions(tokens: Array<out String>?, depth: Int): ArrayList<String> {
         val ret = arrayListOf<String>()
-        AbstractDungeon.player.relics
-            .map { it.relicId.replace(' ', '_') }
+        return AttachDatabase.getRelicsDevCommand(AbstractDungeon.player.chosenClass)
             .toCollection(ret)
-        return ret
     }
 }
